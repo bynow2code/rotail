7bfe5c750bcbc6387c8b8d0c6f79889125465e55
changqianqian <bynow2code@qq.com>
2025-12-03 06:04:51 +0800

    feat(file): 实现文件尾部监听功能
    - 新增 TailFile 结构体及构造函数 NewTailFile
    - 支持通过 WithSeek 选项设置文件读取起始位置
    - 添加文件信息获取方法 GetFileInfo 和 GetSize
    - 实现文件内容逐行读取功能 ReaderLines
    - 集成 fsnotify 监听文件写入和删除事件
    - 处理文件截断情况并重新定位读取位置
    - 增加对文件变化的实时响应逻辑
    - 引入 github.com/fsnotify/fsnotify 和 golang.org/x/sys 依赖库

--- main.go
+++ main.go
@@ -7,50 +7,189 @@ import (
 	"io"
 	"log"
 	"os"
-	"strings"
-	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-func main() {
-	err := tailFile("test.log")
-	if err != nil {
-		log.Fatalln(err)
-	}
+type TailFile struct {
+	FilePath string
+	file     *os.File
 }
 
-func tailFile(filePath string) error {
+type Option func(file *TailFile) error
+
+// WithSeek 设置文件下次读取或写入操作的偏移量
+func WithSeek(offset int64, whence int) Option {
+	return func(tf *TailFile) error {
+		if tf.file == nil {
+			return fmt.Errorf("文件未打开")
+		}
+		_, err := tf.file.Seek(offset, whence)
+		if err != nil {
+			return fmt.Errorf("文件Seek错误: %w", err)
+		}
+		return nil
+	}
+}
+func NewTailFile(filePath string, options ...Option) (*TailFile, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return fmt.Errorf("打开文件错误: %w", err)
+		return nil, fmt.Errorf("打开文件错误: %w", err)
 	}
-	defer file.Close()
 
-	_, err = file.Seek(0, io.SeekEnd)
+	tf := &TailFile{
+		FilePath: filePath,
+		file:     file,
+	}
+
+	for _, option := range options {
+		if err := option(tf); err != nil {
+			return nil, err
+		}
+	}
+
+	return tf, nil
+}
+
+// Close 关闭文件
+func (tf *TailFile) Close() {
+	_ = tf.file.Close()
+}
+
+// GetFileInfo 获取文件信息
+func (tf *TailFile) GetFileInfo() (os.FileInfo, error) {
+	fileInfo, err := tf.file.Stat()
 	if err != nil {
-		return fmt.Errorf("文件Seek错误: %w", err)
+		return nil, fmt.Errorf("获取文件信息错误: %w", err)
 	}
+	return fileInfo, nil
+}
 
-	reader := bufio.NewReader(file)
+// GetSize 获取文件大小
+func (tf *TailFile) GetSize() (int64, error) {
+	fileInfo, err := tf.GetFileInfo()
+	if err != nil {
+		return 0, err
+	}
+	return fileInfo.Size(), nil
+}
 
-	ticker := time.NewTicker(500 * time.Millisecond)
-	for range ticker.C {
-		for {
-			line, err := reader.ReadString('\n')
-			if err != nil && !errors.Is(err, io.EOF) {
-				if !errors.Is(err, io.EOF) {
-					return fmt.Errorf("读取文件错误: %w", err)
-				}
-			}
+// Seek 设置文件下次读取或写入操作的偏移量
+func (tf *TailFile) Seek(offset int64, whence int) (int64, error) {
+	ret, err := tf.file.Seek(offset, whence)
+	if err != nil {
+		return 0, fmt.Errorf("文件Seek错误: %w", err)
+	}
+	return ret, nil
+}
 
-			line = strings.TrimSpace(line)
-			if len(line) > 0 {
-				fmt.Println(line)
-			}
+func (tf *TailFile) ReaderLines() error {
+	reader := bufio.NewReader(tf.file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil && !errors.Is(err, io.EOF) {
+			return fmt.Errorf("读取文件错误: %w", err)
+		}
 
-			if errors.Is(err, io.EOF) {
-				break
-			}
+		fmt.Println(line)
+
+		// 最后一行
+		if errors.Is(err, io.EOF) {
+			break
 		}
 	}
 	return nil
+}
+
+func main() {
+	filePath := "test.log"
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	errCh := make(chan error)
+
+	go func() {
+		tailFile, err := NewTailFile(filePath, WithSeek(0, io.SeekEnd))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer tailFile.Close()
+
+		lastSize, err := tailFile.GetSize()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				log.Println("event:", event)
+
+				if event.Has(fsnotify.Write) {
+					if err := handleWriteEvent(tailFile, &lastSize); err != nil {
+						return
+					}
+				}
+
+				if event.Has(fsnotify.Remove) {
+					handleRemoveEvent(errCh, err)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				handleWatcherError(errCh, err)
+				return
+			}
+		}
+	}()
+
+	<-make(chan struct{})
+}
+
+func handleWriteEvent(tailFile *TailFile, lastSize *int64) error {
+	currSize, err := tailFile.GetSize()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if currSize == *lastSize {
+		// 大小不变，无需处理
+		return nil
+	}
+
+	if currSize < *lastSize {
+		// 文件被截断
+		if _, err := tailFile.Seek(0, io.SeekStart); err != nil {
+			return err
+		}
+	}
+
+	if err := tailFile.ReaderLines(); err != nil {
+		return err
+	}
+
+	*lastSize = currSize
+	return nil
+}
+
+func handleRemoveEvent(errCh chan error, err error) {
+	errCh <- fmt.Errorf("文件被删除: %w", err)
+}
+
+func handleWatcherError(errCh chan error, err error) {
+	errCh <- fmt.Errorf("watcher错误: %w", err)
 }
